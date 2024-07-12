1c9db95d733666ba042732aaa49398ac9bff16f9
Olooce <asirastephen9@gmail.com>
2024-07-12 10:40:00 +0300

    refactor: optimize code

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:basic_quiz_app/data/questions.dart';
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key, required this.selectedAnswers});
@@ -7,6 +8,16 @@ class ResultsScreen extends StatelessWidget {
 
   List<Map<String, Object>> getSummary() {
     final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < selectedAnswers.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': selectedAnswers[i],
+      });
+    }
+
     return summary;
   }
 
