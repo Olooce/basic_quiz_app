b9c88655c07f306f2346c1e4d01933ba3b91092a
Olooce <asirastephen9@gmail.com>
2024-08-09 13:10:01 +0300

    More updates

--- lib/main.dart
+++ lib/main.dart
@@ -4,27 +4,3 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const Quiz());
 }
-
-// import 'package:basic_quiz_app/quiz_app.dart';
-
-// void main() {
-//   runApp(
-//     MaterialApp(
-//       home: Scaffold(
-//         body: Container(
-//           decoration: const BoxDecoration(
-//             gradient: LinearGradient(
-//               colors: [
-//                 Color.fromARGB(255, 54, 67, 244),
-//                 Color.fromARGB(255, 54, 67, 244)
-//               ],
-//               begin: Alignment.topLeft,
-//               end: Alignment.bottomRight,
-//             ),
-//           ),
-//           child: const QuizApp(),
-//         ),
-//       ),
-//     ),
-//   );
-// }
